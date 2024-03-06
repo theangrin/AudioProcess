405b3957face2efbb473de7b67c5d185eaf625f9
YangHan <yhan234@qq.com>
2024-03-06 10:04:26 +0800

    add extract_audio_from_video function

--- app/algorithms/__init__.py
+++ app/algorithms/__init__.py
@@ -1,5 +1,13 @@
 from app.algorithms.ernie import Ernie
 from app.algorithms.asr import ASR, Sentence, AsrResult
 from app.algorithms.fake_asr import FakeASR
+from app.algorithms.utils import extract_audio_from_video
 
-__all__ = ["Ernie", "ASR", "Sentence", "AsrResult", "FakeASR"]
+__all__ = [
+    "Ernie",
+    "ASR",
+    "Sentence",
+    "AsrResult",
+    "extract_audio_from_video",
+    "FakeASR",
+]
